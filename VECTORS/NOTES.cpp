699ea8f9505850e797b2c2f48dbfa3b71b1ed8dd
KhemeshAwana63 <khemeshawana2005@gmail.com>
2025-03-08 22:08:19 +0530

    added more notes in vectors

--- VECTORS/NOTES.cpp
+++ VECTORS/NOTES.cpp
@@ -8,14 +8,20 @@ int main(){
     vector <int> tony = {12,4,65,44}; // size = 4
     vector <int> karan(5,0); // size = 5 and values are 0
     cout << tony[0] << endl;
-    cout << karan[3] << endl;
+    karan.push_back(8);
+
+    
+    cout << karan.at(5) << endl;
     for(int val : tony ) cout << val << " ";  // here the vlaue of vector is extracted for each index just like python
     return 0;
 }
 //FUNCTIONS
 /*
-    
-
-
-
+    size
+    push_back
+    pop_back
+    front
+    back
+    at
+    capacity
 */
