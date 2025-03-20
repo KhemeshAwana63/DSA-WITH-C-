1e1c9c1f5fa1f461c09fee70c1aa77a94946c7a4
KhemeshAwana63 <khemeshawana2005@gmail.com>
2025-03-20 05:46:28 +0530

    adding the notes of time complexity

---
+++ DATA STRUCTURE/TIME&SPACE_COMPLEXITY/notes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// time complexity -> not the time taken but the time as a function of imput size
+// big(o) notation -> time complexity for the worst case scenerio
+//there are more notations for best case(theta) , average case(omega) etc
+//space complexity -> amount of space taken as a function of input size
+// -> input space(not included) -> auxilliery space(included)
+int main(){
+
+    return 0;
+}
